d83bdd8370f980aacfe8dc9c3caff5ec34465196
Molly Shove <pixieofhugs@gmail.com>
2018-04-23 09:56:40 -0700

    Day two of the new flask mega-tutorial. Learned about and made templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'pixie'}
+    tasks = [
+        {
+            'author':{'username': 'Loki'},
+            'body': 'I did something tricky!'
+        },
+        {
+            'author': {'username': 'Bex'},
+            'body': 'I did something heroic!'
+        }
+
+    ]
+    return render_template('index.html', title = 'Home', user = user, tasks = tasks)
