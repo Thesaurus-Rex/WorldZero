55691c3db79566978c4ce793b435cac101488e28
Molly Shove <pixieofhugs@gmail.com>
2018-04-24 16:57:53 -0700

    Day three! Part two. Spelling mistakes are the bane of my existence. It works now

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,14 @@ from flask import render_template
 from app.forms import LoginForm
 
 @app.route('/')
+
+
 @app.route('/login')
 def login():
     form =  LoginForm()
     return render_template('login.html', title='Sign In', form = form)
+
+
 @app.route('/index')
 def index():
     user = {'username': 'pixie'}
